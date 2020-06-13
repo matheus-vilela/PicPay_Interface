f223f4bd3cf59a551932ec884116aed37bd5e1a5
vilelagit <vilela.matheus@outlook.com>
2020-06-13 18:02:54 -0300

    Adicionada interface Home, Wallet, Pay

---
+++ src/screens/Pay/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+export default function Pay (){
+  return(
+    <View>
+       <Text>TESTE TESTE TESTE</Text>
+    </View>
+  );
+}
